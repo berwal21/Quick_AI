326523e18174a1359311f075be44671b25219314
berwal21 <amanberwal51@gmail.com>
2025-08-22 09:42:57 +0530

    authentication and home page done

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import AiTools from "../components/AiTools";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <AiTools />
+    </>
+  );
+};
+
+export default Home;
+/*lucide-react is a library for icons, 
+clerk is a user authentication service and billing platform,
+*/
