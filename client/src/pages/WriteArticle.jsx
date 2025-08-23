43dcbece8945df95fc17e8f2064eebbcf3e97b7b
berwal21 <amanberwal51@gmail.com>
2025-08-23 21:37:15 +0530

    add write article page

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -1,7 +1,81 @@
+import { Edit, Sparkles } from "lucide-react";
 import React from "react";
 
 const WriteArticle = () => {
-  return <div>WriteArticle</div>;
+  const articleLength = [
+    { length: 800, text: "Short (500-800 words)" },
+    { length: 1500, text: "Medium (1000-1500 words)" },
+    { length: 2500, text: "Long (2000-2500 words)" },
+  ];
+
+  const [selectedLength, setSelectedLength] = React.useState(articleLength[0]);
+  const [input, setInput] = React.useState("");
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="h-full overflow-y-scroll p-6 flex flex-wrap items-start gap-4 text-slate-700">
+      {/* left col */}
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#4A7AFF]" />
+          <h1 className="text-xl font-semibold">Artilce Configuration</h1>
+        </div>
+        <p className="mt-6 text-sm font-medium">Artilce Topic</p>
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+          type="text"
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
+          placeholder="The future of atrificial intelligence is..."
+          required
+        />
+
+        <p className="mt-4 text-sm font-medium">Article Length</p>
+        <div className="mt-3 flex gap-3 flex-wrap sm:max-w-9/11">
+          {articleLength.map((item, index) => (
+            <span
+              onClick={() => setSelectedLength(item)}
+              className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${
+                selectedLength.text === item.text
+                  ? "bg-blue-50 text-blue-700"
+                  : "text-gray-500 border-gray-300"
+              }`}
+              key={index}
+            >
+              {item.text}
+            </span>
+          ))}
+        </div>
+        <br />
+        <button
+          className="w-full flex justify-center items-center gap-2
+        bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer hover:opacity-90"
+        >
+          <Edit className="w-5" /> Generate Article
+        </button>
+      </form>
+      {/* right col */}
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]">
+        <div className="flex items-center gap-3">
+          <Edit className="w-5 h-5 text-[#4A7AFF]" />
+          <h1 className="text-xl font-semibold">Generated Article</h1>
+        </div>
+
+        <div className="flex-1 flex items-center justify-center">
+          <div className="text-sm flex flex-col items-center gap-5">
+            <Edit className="w-9 h-9" />
+            <p>Enter a topic and click "Generate article" to get started</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WriteArticle;
