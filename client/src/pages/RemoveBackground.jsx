17926cb8dd475d78d778f59252619350c2db4264
berwal21 <amanberwal51@gmail.com>
2025-08-24 10:46:07 +0530

    add remove background page into ui

--- client/src/pages/RemoveBackground.jsx
+++ client/src/pages/RemoveBackground.jsx
@@ -1,7 +1,57 @@
+import { Eraser, Sparkles } from "lucide-react";
 import React from "react";
 
 const RemoveBackground = () => {
-  return <div>RemoveBackground</div>;
+  const [input, setInput] = React.useState("");
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="h-full overflow-y-scroll p-6 flex flex-wrap items-start gap-4 text-slate-700">
+      {/* left col */}
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#FF4938]" />
+          <h1 className="text-xl font-semibold">Background Removal</h1>
+        </div>
+        <p className="mt-6 text-sm font-medium">Upload image</p>
+        <input
+          onChange={(e) => setInput(e.target.files[0])}
+          accept="image/*"
+          type="file"
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600"
+          required
+        />
+        <p className="text-xs text-gray-500 font-light mt-1">
+          Supports JPG, PNG, and other image formats
+        </p>
+        <button
+          className="w-full flex justify-center items-center gap-2
+        bg-gradient-to-r from-[#F6AB41] to-[#FF4938] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer hover:opacity-90"
+        >
+          <Eraser className="w-5" /> Remove Background
+        </button>
+      </form>
+      {/* right col */}
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96">
+        <div className="flex items-center gap-3">
+          <Eraser className="w-5 h-5 text-[#FF4938]" />
+          <h1 className="text-xl font-semibold">Processed Image</h1>
+        </div>
+
+        <div className="flex-1 flex items-center justify-center">
+          <div className="text-sm flex flex-col items-center gap-5">
+            <Eraser className="w-9 h-9" />
+            <p>Uplaod an image and click "Remove Background" to get started</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RemoveBackground;
