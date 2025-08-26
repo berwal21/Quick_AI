23560ca93187e07a5ff55ae4f6a89110f5f3ad1c
berwal21 <amanberwal51@gmail.com>
2025-08-26 20:41:39 +0530

    implement generate image function

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,6 +1,8 @@
 import OpenAI from "openai";
 import sql from "../config/db.js";
 import { clerkClient } from "@clerk/express";
+import axios from "axios";
+import { v2 as cloudinary } from "cloudinary";
 
 const AI = new OpenAI({
   apiKey: process.env.GEMINI_API_KEY,
@@ -89,6 +91,49 @@ export const generateBlogTitle = async (req, res) => {
       });
     }
     res.json({ success: true, content });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const generateImage = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { prompt, publish } = req.body;
+
+    const plan = req.plan;
+
+    if (plan !== "premium") {
+      return res.json({
+        success: false,
+        message: "This feature is only available for premium subcription.",
+      });
+    }
+
+    const formData = new FormData();
+    formData.append("prompt", prompt);
+    const { data } = await axios.post(
+      "https://clipdrop-api.co/text-to-image/v1",
+      formData,
+      {
+        headers: { "x-api-key": process.env.CLIPDROP_API_KEY },
+        responseType: "arraybuffer",
+      }
+    );
+
+    const base64Image = `data:image/png;base64,${Buffer.from(
+      data,
+      "binary"
+    ).toString("base64")}`;
+
+    const { secure_url } = await cloudinary.uploader.upload(base64Image);
+
+    await sql`INSERT INTO creations (user_id, prompt, content, type, publish) VALUES (${userId}, ${prompt}, ${content}, 'image', ${
+      publish ?? false
+    })`;
+
+    res.json({ success: true, content: secure_url });
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ success: false, message: error.message });
