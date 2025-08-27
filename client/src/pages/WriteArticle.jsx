3d41d35e472f77704c4b31f57c367600c8e09992
berwal21 <amanberwal51@gmail.com>
2025-08-27 21:29:32 +0530

    feat: add article generation functionality with AI and display results in Markdown

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -1,5 +1,11 @@
 import { Edit, Sparkle } from "lucide-react";
 import React from "react";
+import axios from "axios";
+import { useAuth } from "@clerk/clerk-react";
+import { toast } from "react-hot-toast";
+import Markdown from "react-markdown";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const WriteArticle = () => {
   const articleLength = [
@@ -10,9 +16,35 @@ const WriteArticle = () => {
 
   const [selectedLength, setSelectedLength] = React.useState(articleLength[0]);
   const [input, setInput] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+  const [content, setContent] = React.useState("");
 
-  const onSubmitHandler = (e) => {
+  const { getToken } = useAuth();
+
+  const onSubmitHandler = async (e) => {
     e.preventDefault();
+
+    try {
+      setLoading(true);
+      const prompt = `Write an article about ${input} in ${selectedLength.text}`;
+      const { data } = await axios.post(
+        "/api/ai/generate-article",
+        { prompt, length: selectedLength.length },
+        {
+          headers: { Authorization: `Bearer ${await getToken()}` },
+        }
+      );
+
+      if (data.success) {
+        setContent(data.content);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+
+    setLoading(false);
   };
 
   return (
@@ -54,10 +86,16 @@ const WriteArticle = () => {
         </div>
         <br />
         <button
+          disabled={loading}
           className="w-full flex justify-center items-center gap-2
         bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer hover:opacity-90"
         >
-          <Edit className="w-5" /> Generate Article
+          {loading ? (
+            <span className="w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin"></span>
+          ) : (
+            <Edit className="w-5" />
+          )}
+          Generate Article
         </button>
       </form>
       {/* right col */}
@@ -67,12 +105,20 @@ const WriteArticle = () => {
           <h1 className="text-xl font-semibold">Generated Article</h1>
         </div>
 
-        <div className="flex-1 flex items-center justify-center">
-          <div className="text-sm flex flex-col items-center gap-5">
-            <Edit className="w-9 h-9" />
-            <p>Enter a topic and click "Generate article" to get started</p>
+        {!content ? (
+          <div className="flex-1 flex items-center justify-center">
+            <div className="text-sm flex flex-col items-center gap-5">
+              <Edit className="w-9 h-9" />
+              <p>Enter a topic and click "Generate article" to get started</p>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="mt-3 h-full overflow-y-scroll text-sm text-slate-600">
+            <div className="reset-tw">
+              <Markdown>{content}</Markdown>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
