077fb29b58813911f0e833a1eff304148ce9991a
berwal21 <amanberwal51@gmail.com>
2025-08-27 11:24:04 +0530

    add user routes and connect to Cloudinary in server setup

--- server/server.js
+++ server/server.js
@@ -2,9 +2,13 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { clerkMiddleware, requireAuth } from "@clerk/express";
-import aiRouter from "./routes/aiRoutes";
+import aiRouter from "./routes/aiRoutes.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userrouter from "./routes/userRoutes.js";
 
 const app = express();
+
+await connectCloudinary();
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +19,7 @@ app.get("/", (req, res) => res.send("Server is Live"));
 app.use(requireAuth());
 
 app.use("/api/ai", aiRouter);
+app.use("/api/user", userrouter);
 
 const PORT = process.env.PORT || 3000;
 
