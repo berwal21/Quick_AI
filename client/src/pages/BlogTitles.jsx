ff6d0a873de918269bad090fee1aa0b676f1df06
berwal21 <amanberwal51@gmail.com>
2025-08-27 21:29:11 +0530

    feat: implement blog title generation with AI and display results

--- client/src/pages/BlogTitles.jsx
+++ client/src/pages/BlogTitles.jsx
@@ -1,5 +1,12 @@
 import { Hash, Sparkles } from "lucide-react";
 import React from "react";
+import axios from "axios";
+import Markdown from "react-markdown";
+import { useAuth } from "@clerk/clerk-react";
+
+import toast from "react-hot-toast";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const BlogTitles = () => {
   const blogCatrgories = [
@@ -15,9 +22,32 @@ const BlogTitles = () => {
 
   const [selectedCategory, setSelectedCategory] = React.useState("General");
   const [input, setInput] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+  const [content, setContent] = React.useState("");
 
-  const onSubmitHandler = (e) => {
+  const { getToken } = useAuth();
+
+  const onSubmitHandler = async (e) => {
     e.preventDefault();
+    try {
+      setLoading(true);
+      const prompt = `Generate a blog title for the keyword ${input} in the category ${selectedCategory}`;
+
+      const { data } = await axios.post(
+        "/api/ai/generate-blog-title",
+        { prompt },
+        { headers: { Authorization: `Bearer ${await getToken()}` } }
+      );
+
+      if (data.success) {
+        setContent(data.content);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+    setLoading(false);
   };
   return (
     <div className="h-full overflow-y-scroll p-6 flex flex-wrap items-start gap-4 text-slate-700">
@@ -58,10 +88,16 @@ const BlogTitles = () => {
         </div>
         <br />
         <button
+          disabled={loading}
           className="w-full flex justify-center items-center gap-2
         bg-gradient-to-r from-[#C341F6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer hover:opacity-90"
         >
-          <Hash className="w-5" /> Generate Title
+          {loading ? (
+            <span className="w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin"></span>
+          ) : (
+            <Hash className="w-5" />
+          )}
+          Generate Title
         </button>
       </form>
       {/* right col */}
@@ -71,12 +107,20 @@ const BlogTitles = () => {
           <h1 className="text-xl font-semibold">Generated titles</h1>
         </div>
 
-        <div className="flex-1 flex items-center justify-center">
-          <div className="text-sm flex flex-col items-center gap-5">
-            <Hash className="w-9 h-9" />
-            <p>Enter a topic and click "Generate title" to get started</p>
+        {!content ? (
+          <div className="flex-1 flex items-center justify-center">
+            <div className="text-sm flex flex-col items-center gap-5">
+              <Hash className="w-9 h-9" />
+              <p>Enter a topic and click "Generate title" to get started</p>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="mt-3 h-full overflow-y-scroll text-sm text-slate-600">
+            <div className="reset-tw">
+              <Markdown>{content}</Markdown>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
