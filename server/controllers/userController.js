bcbdc701e5c0debec63f240f2bbe0f1784e1ef34
berwal21 <amanberwal51@gmail.com>
2025-08-27 11:16:24 +0530

    add user creation endpoints and toggle like functionality

---
+++ server/controllers/userController.js
@@ -0,0 +1,59 @@
+import sql from "../config/db.js";
+
+export const getUserCreations = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+
+    const creations =
+      await sql`SELECT * FROM creations WHERE user_id = ${userId} ORDER BY created_at DESC`;
+
+    res.json({ success: true, creations });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getPublishCreations = async (req, res) => {
+  try {
+    const creations =
+      await sql`SELECT * FROM creations WHERE publish = true ORDER BY created_at DESC`;
+
+    res.json({ success: true, creations });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const toggleLikeCreation = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { id } = req.body;
+
+    const [creation] = await sql`SELECT * FROM creations WHERE id = ${id}`;
+    if (!creation)
+      return res.json({ success: false, message: "Creation not found" });
+
+    const currentLikes = creation.likes;
+    const userIdStr = userId.toString();
+    let updatedLikes;
+    let message;
+
+    if (currentLikes.includes(userIdStr)) {
+      // User has already liked, so remove the like
+      updatedLikes = currentLikes.filter((uid) => uid !== userIdStr);
+      message = "Creation Unliked ";
+    } else {
+      // User has not liked yet, so add the like
+      updatedLikes = [...currentLikes, userIdStr];
+      message = "Creation Liked ";
+    }
+
+    const formattedArray = `{${updatedLikes.join(",")}}`;
+
+    await sql`UPDATE creations SET likes = ${formattedArray}::text[] WHERE id = ${id}`;
+
+    res.json({ success: true, message });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
